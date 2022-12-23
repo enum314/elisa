0ddfe8235a22f0a4d491e029cee3b43742b6b87c
enum314 <enum314@gmail.com>
2022-12-23 18:19:10 +0800

    feat(AppRouter): add adminRouter

--- src/server/routers/app.ts
+++ src/server/routers/app.ts
@@ -1,14 +1,16 @@
 import { router } from '@server/trpc';
 
+import { adminRouter } from './admin';
 import { messageRouter } from './message';
 import { profileRouter } from './profile';
 import { siteRouter } from './site';
 import { wsRouter } from './ws';
 
 export const appRouter = router({
-	site: siteRouter,
-	profile: profileRouter,
+	admin: adminRouter,
 	message: messageRouter,
+	profile: profileRouter,
+	site: siteRouter,
 	ws: wsRouter,
 });
 
