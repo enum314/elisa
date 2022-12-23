90b051b2f2081a0e8983eed130a56c71e97bfdea
enum314 <enum314@gmail.com>
2022-12-23 18:17:37 +0800

    feat(GlobalChat): add profanity blocking

--- src/server/routers/message.ts
+++ src/server/routers/message.ts
@@ -10,21 +10,41 @@ export const messageRouter = router({
 	send: authProcedure
 		.use(ratelimit('message', { points: 5, duration: 15 }))
 		.use(withProfile)
-		.input(z.object({ content: z.string().min(1).max(100) }).strict())
+		.input(
+			z
+				.object({
+					content: z.string().min(1).max(100),
+				})
+				.strict(),
+		)
 		.mutation(async ({ input, ctx }) => {
+			if (!ctx.siteSettings.globalChat) {
+				throw new TRPCError({
+					code: 'FORBIDDEN',
+					message: 'It seems an administrator disabled global chat.',
+				});
+			}
+
 			if (!ctx.profile.globalChat) {
-				throw new TRPCError({ code: 'FORBIDDEN' });
+				throw new TRPCError({
+					code: 'FORBIDDEN',
+					message:
+						'It seems you have global chat turned off. You can turn it back on at Account Settings',
+				});
 			}
 
 			const content = input.content.trim();
 
-			// TODO: Admin dashboard, blocking profanity
-			// if (ctx.filter.isProfane(content)) {
-			// 	throw new TRPCError({
-			// 		code: 'BAD_REQUEST',
-			// 		message: 'Woah chill, this is a profanity-free environment',
-			// 	});
-			// }
+			if (
+				ctx.siteSettings.globalChatBlockProfanity &&
+				ctx.filter.isProfane(content)
+			) {
+				throw new TRPCError({
+					code: 'BAD_REQUEST',
+					message:
+						'Woah chill, this is a family friendly environment.',
+				});
+			}
 
 			const now = new Date();
 			websocket.emit('message', {
